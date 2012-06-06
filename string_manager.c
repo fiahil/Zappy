0ce3502bdf27622d9a09b08bc61c70ef2389b3c8
busina_b <benjamin.businaro@gmail.com>
2012-06-06 13:58:02 +0200

    merge

--- string_manager.c
+++ string_manager.c
@@ -5,11 +5,11 @@
 ** Login   <busina_b@epitech.net>
 ** 
 ** Started on Wed Jun  6 11:42:13 2012 benjamin businaro
-** Last update Wed Jun  6 12:04:12 2012 benjamin businaro
+** Last update Wed Jun  6 13:57:12 2012 benjamin businaro
 */
 
 
-void	get_commands(t_clientManager *client, char *cmd)
+void	get_commands(t_clientManager client, char *cmd)
 {
   // split de cmd en fonction du mode
   // stock de la fin de cmd dans current pour concatenation

581ee5d246e8fcfc90c1912f21702cbd13a5e881
Fiahil <fiahil@gmail.com>
2012-06-06 22:02:09 +0200

    modif main

--- src/main.c
+++ src/main.c
@@ -7,6 +7,15 @@
 #include <stdlib.h>
 #include "def.h"
 #include "get_arg.h"
+#include "netManager.h"
+
+void		run(void)
+{
+  while (666)
+  {
+    iterClient();
+  }
+}
 
 int		main(int ac, char **av)
 {
@@ -14,5 +23,8 @@ int		main(int ac, char **av)
 
   if (get_arg(ac, av, &args) == -1)
     return (EXIT_FAILURE);
+  setConnections(/* port ? */);
+  initClientTab();
+  run();
   return (EXIT_SUCCESS);
 }

4b783b59076c38ac1348969ce7d2f44eba5d3c67
Fiahil <fiahil@gmail.com>
2012-06-07 09:50:41 +0200

    manager

---
+++ headers/server_routine.h
@@ -0,0 +1,14 @@
+/*
+ * Fiahil
+ * 07.06.2012
+ */
+
+#ifndef __SERVER_ROUTINE_H__
+#define __SERVER_ROUTINE_H__
+
+#include "def.h"
+
+void	server_routine_input(t_clientManager);
+void	server_routine_output(t_clientManager);
+
+#endif /* __SERVER_ROUTINE_H__ */

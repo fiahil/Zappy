99615936f47f87737d8295ef5fde4ed6465fdefe
Fiahil <fiahil@gmail.com>
2012-06-08 09:35:56 +0200

    ajout epoll

---
+++ headers/epoll_manager.h
@@ -0,0 +1,14 @@
+/*
+ * Fiahil
+ * 07.06.2012
+ */
+
+#ifndef __EPOLL_MANAGER_H__
+#define __EPOLL_MANAGER_H__
+
+void		init_epoll();
+void		add_monitor();
+void		del_monitor();
+void		update_monitor();
+
+#endif /* __EPOLL_MANAGER_H__ */
---
+++ src/epoll_manager.c
@@ -0,0 +1,40 @@
+/*
+ * Fiahil
+ * 07.06.2012
+ */
+
+#include <sys/epoll.h>
+#include "error.h"
+#include "epoll_manager.h"
+
+static int			g_fd = 0;
+static struct epoll_event	*g_events = NULL;
+
+void		init_epoll()
+{
+  g_fd = epoll_create1(0);
+  if (g_fd == -1)
+    handleError("epoll_create", "fail", 0);
+}
+
+void		add_monitor(int fd)
+{
+  struct epoll_event	event;
+
+  event.event = EPOLLIN | EPOLLOUT;
+  event.data.fd = fd;
+  if (epoll_ctl(g_fd, EPOLL_CTL_ADD, fd, &event) == -1)
+    handleError("epoll_ctl", "add", 0);
+}
+
+void		del_monitor(int fd)
+{
+  if (epoll_ctl(g_fd, EPOLL_CTL_DEL, fd, (void*)-1) == -1)
+    handleError("epoll_ctl", "del", 0);
+}
+
+void		update_monitor()
+{
+  if (epoll_wait(g_fd, g_events, 10000, 0) == -1)
+    handleError("epoll_wait", "fail", 0);
+}

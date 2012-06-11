c648b48d3ec815d54b8f704736c22406619ed137
pierre martin <mart_i@pc-mart-i.(none)>
2012-06-11 10:12:33 +0200

    correction des frees des listes chainees

--- lists/base/desalloc.c
+++ lists/base/desalloc.c
@@ -5,7 +5,7 @@
 ** Login   <mart_i@epitech.net>
 ** 
 ** Started on Thu Apr 19 09:21:42 2012 pierre martin
-** Last update Thu Jun  7 19:50:24 2012 pierre martin
+** Last update Mon Jun 11 10:11:41 2012 pierre martin
 */
 
 #include	<stdlib.h>
@@ -28,7 +28,7 @@ void		delete_iters(t_iter *this, t_dtor dtor)
 {
   if (this)
     {
-      if (this->next && dtor)
+      if (this->next)
 	delete_iters(this->next, dtor);
       delete_iter(this, dtor);
     }

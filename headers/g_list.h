af46f58d0f5879dc8288bb2239449f3e10afa73c
Fiahil <fiahil@gmail.com>
2012-06-12 21:27:57 +0200

    g_list header

---
+++ headers/g_list.h
@@ -0,0 +1,13 @@
+/*
+ * Fiahil
+ * 12.06.2012
+ */
+
+#ifndef __G_LIST_H__
+#define __G_LIST_H__
+
+t_list	*get_action_list();
+t_list	*get_send_list();
+void	flush_send_list();
+
+#endif /* __G_LIST_H__ */

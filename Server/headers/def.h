5951085b11f193e534a9bfd37cdbe51fb3582770
Sullivan <Sulli@sulli.(none)>
2012-06-14 18:57:39 +0200

    Corrected segfault in fill_map. Algorithm needs to be reviewed.

--- Server/headers/def.h
+++ Server/headers/def.h
@@ -74,8 +74,7 @@ typedef enum
     SIBUR,
     MENDIANE,
     PHIRAS,
-    THYSTAME,
-    LAST
+    THYSTAME
   } t_resource;
 
 typedef enum
@@ -111,7 +110,7 @@ struct s_map
 struct s_inventory
 {
   t_bool	status;
-  int		resources[LAST];
+  int		resources[7];
 };
 
 struct s_square

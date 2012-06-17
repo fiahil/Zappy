7ff9effa73582d26aff49a31bd2a70a56ef13a7f
busina_b <benjamin.businaro@gmail.com>
2012-06-17 15:50:05 +0200

    add de iter_function{c.h}pp

---
+++ Server/headers/iter_function.h
@@ -0,0 +1,20 @@
+/*
+** iter_function.h for Zappy in /home/busina_b/Projet/Zappy-Unix/Zappy/Server/headers/
+** 
+** Made by benjamin businaro
+** Login   <busina_b@epitech.net>
+** 
+** Started on  Sat Jun 16 16:16:43 2012 benjamin businaro
+** Last update Sat Jun 16 16:20:50 2012 benjamin businaro
+*/
+
+#ifndef __ITER_FUNCTION_H__
+#define __ITER_FUNCTION_H__
+
+#include "def.h"
+
+int		action_cleaner(void *, size_t );
+t_player	init_player();
+
+#endif /* __ITER_FUNCTION_H__ */
+

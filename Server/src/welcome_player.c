93256215de8e7c5107c9639a6e60ffba15bcab85
ulric lefevre <lefevr_u@Atlantis.(none)>
2012-06-19 19:12:00 +0200

    Gestion des équipes -> envoi des données d'accueil

--- Server/src/welcome_player.c
+++ Server/src/welcome_player.c
@@ -1,9 +1,12 @@
 
+#define		_GNU_SOURCE
+
 #include	<stdio.h>
 #include	<stdlib.h>
 #include	<string.h>
 
 #include	"def.h"
+#include	"map.h"
 #include	"algorithm.h"
 
 static int	func_cmp(void *s1, size_t s1len, void *s2, size_t s2len)
@@ -15,8 +18,6 @@ static int	func_cmp(void *s1, size_t s1len, void *s2, size_t s2len)
 
 static int	chk_team(t_data_serv server, char *data)
 {
-  // chk s'il reste de la place dans l'equipe
-  // return le nombre de client restant
   t_iter	*it;
 
   if ((it = list_find_cmp(server->teams, &func_cmp, data, 0)) == NULL)
@@ -28,19 +29,23 @@ static int	chk_team(t_data_serv server, char *data)
 
 t_bool		welcome_player(t_data_serv server, t_player player, char *data)
 {
-  // return false s'il faut delete le player
-  // passer en accueilli le player -> et welcomed a true
   int		nb_client;
+  char		*str;
+  t_map		map;
 
   if (data)
     {
       if ((nb_client = chk_team(server, data)) == -1)
 	return (FALSE);
       printf("test nb_client : %d\n", nb_client); // TODO affichage tmp
-      //envoi du nombre de client restant
-      list_push_back_new(player->cm.out, "\n", strlen("\n") + 1);
-      //envoi de la taille du monde
-      list_push_back_new(player->cm.out, "\n", strlen("\n") + 1);
+      asprintf(&str, "%d\n", nb_client);
+      list_push_back_new(player->cm.out, str, strlen(str) + 1);
+      free(str);
+      map = get_map(NULL);
+      asprintf(&str, "%d %d\n", map->size_x, map->size_y);
+      list_push_back_new(player->cm.out, str, strlen(str) + 1);
+      free(str);
+      player->welcome = TRUE;
     }
   return (TRUE);
 }

a1f120db508600144051e7d5d93ae3ee2385003d
Fiahil <fiahil@gmail.com>
2012-06-20 17:05:42 +0200

    select ecriture

--- Server/src/select_manager.c
+++ Server/src/select_manager.c
@@ -19,7 +19,11 @@ static void	select_set(void *ptr, size_t s)
 {
   (void)s;
   if ((*(t_player*)ptr)->cm.online)
+  {
     FD_SET((*(t_player*)ptr)->cm.sock.fd, &g_sm->rds);
+    if (!(*(t_player*)ptr)->cm.out->empty)
+      FD_SET((*(t_player*)ptr)->cm.sock.fd, &g_sm->wds);
+  }
 }
 
 static void	select_cmp(void *ptr, size_t s)
@@ -40,6 +44,7 @@ void		select_manager(t_data_serv ds, t_select_manager sm)
 {
   g_sm = sm;
   FD_ZERO(&sm->rds);
+  FD_ZERO(&sm->wds);
   FD_SET(ds->sock.fd, &sm->rds);
   list_for_each(ds->player, &select_set);
   if (select(sm->max_fd + 1, &sm->rds, NULL, NULL, &sm->timeout) < 0)

c31cb400c0fa912fbf6ebc27d667f0a793097587
Mister Jingles <ulric.lefevre@gmail.com>
2012-06-21 19:01:17 +0200

    Init_egg -> TODO process egg

--- Server/src/egg_manager.c
+++ Server/src/egg_manager.c
@@ -3,8 +3,15 @@
 #include	<stdlib.h>
 
 #include	"def.h"
+#include	"player.h"
+
+static int	g_id_egg = 0;
 
 void		init_egg(t_egg egg)
 {
-  (void)egg;
+  egg->status = FALSE;
+  egg->id = g_id_egg++;
+  egg->fetus = create_player();
+  egg->timeout.tv_usec = 0; // TODO
+  egg->timeout.tv_sec = 3; // TODO
 }

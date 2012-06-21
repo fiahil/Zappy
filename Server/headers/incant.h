2473d1d8220e63f3dee828387fb4ef231682a6e6
lachet_s <Sulli@sulli.(none)>
2012-06-21 19:44:02 +0200

    Incantion OK - reste a supprimer les ressources utilisees

--- Server/headers/incant.h
+++ Server/headers/incant.h
@@ -5,6 +5,8 @@ size_t	check_players(t_list *, int);
 void	level_up(t_square);
 void	fill_hashcode(t_hash, t_square);
 t_bool	incant_is_ok(t_incant);
-t_bool	init_incant(t_incant, t_player, t_square);
+t_bool	init_incant(t_incant, t_player, t_square, int);
+void	iter_incant(void *, size_t);
+int	incant_cleaner(void *, size_t);
 
 #endif
--- Server/src/incantation_funcs.c
+++ Server/src/incantation_funcs.c
@@ -1,7 +1,14 @@
+#include <time.h>
+
 #include <stdio.h>
 #include <string.h>
 #include "def.h"
 #include "map.h"
+#include "clock.h"
+
+/*
+/!\ PLUS DE 5 FONCTIONS !!! /!\
+*/
 
 static t_u_hash	g_hash_cmp[] = 
   {
@@ -29,16 +36,20 @@ size_t	check_players(t_list *players, int lvl)
   return (count);
 }
 
-void	level_up(t_square cell)
+void	level_up(t_incant incant)
 {
   t_iter	*it;
+  t_map		map;
 
-  it = cell->players->head;
+  map = get_map(NULL);
+  it = map->map[incant->pos.y][incant->pos.x]->players->head;
   while (it)
     {
       ++(*(t_player *)(it->data))->lvl;
       it = it->next;
     }
+  printf("Level up\n");
+  // Remove used resources
 }
 
 void	fill_hashcode(t_hash hashcode, t_square cell)
@@ -60,7 +71,7 @@ t_bool	incant_is_ok(t_incant incant)
   if (incant->status == FALSE)
     return (FALSE);
   map = get_map(NULL);
-  if (check_players(map->map[incant->pos.y][incant->pos.x]->players , incant->incantor->lvl) != map->map[incant->pos.y][incant->pos.x]->players->size)
+  if (check_players(map->map[incant->pos.y][incant->pos.x]->players, incant->incantor->lvl) != map->map[incant->pos.y][incant->pos.x]->players->size)
     {
       incant->status = FALSE;
       return (FALSE);
@@ -71,11 +82,8 @@ t_bool	incant_is_ok(t_incant incant)
   return (incant->status);
 }
 
-t_bool	init_incant(t_incant incant, t_player play, t_square cell)
+t_bool	init_incant(t_incant incant, t_player play, t_square cell, int t)
 {
-  int	i;
-
-  i = -1;
   memset(incant, 0, sizeof(*incant));
   incant->status = TRUE;
   if (check_players(cell->players, play->lvl) != cell->players->size)
@@ -87,10 +95,10 @@ t_bool	init_incant(t_incant incant, t_player play, t_square cell)
   incant->pos.x = play->pos.x;
   incant->pos.y = play->pos.y;
   incant->incantor = play;
-  //incant->timeout = ?;
+  get_current_time(&incant->timeout);
+  incant->timeout.tv_sec += 300 / t;
   fill_hashcode(&incant->hashcode, cell);
-  while (++i < 7 && memcmp(&incant->hashcode, &g_hash_cmp[i], sizeof(g_hash_cmp[i])));
-  if (i == 7)
+  if (memcmp(&incant->hashcode, &g_hash_cmp[play->lvl - 1], sizeof(incant->hashcode)))
     {
       // TEST DISPLAY
       printf("Resources not good\n");
@@ -109,4 +117,28 @@ t_bool	init_incant(t_incant incant, t_player play, t_square cell)
 	 incant->pos.x,
 	 incant->pos.y);
   return (TRUE);
+}
+
+void		iter_incant(void *ptr, size_t s)
+{
+  t_u_timeval	current;
+
+  (void)s;
+  get_current_time(&current);
+  if (!(((t_incant)ptr)->timeout.tv_sec ^ current.tv_sec))
+    {
+      if (incant_is_ok((t_incant)ptr))
+	level_up((t_incant)ptr);
+      else
+	printf("Incant couldn't be performed\n");
+      ((t_incant)ptr)->timeout.tv_sec = 0;    
+    }
+}
+
+int		incant_cleaner(void *ptr, size_t s)
+{
+  (void)s;
+  if (!((t_incant)ptr)->timeout.tv_sec)
+    return (1);
+  return (0);
 }

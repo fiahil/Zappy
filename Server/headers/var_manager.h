505466db523591ace8b96313eea3b7d954d58efc
Mister Jingles <ulric.lefevre@gmail.com>
2012-06-23 18:25:13 +0200

    add headers/var_manager.h src/var_manager.c

---
+++ Server/headers/var_manager.h
@@ -0,0 +1,8 @@
+
+#ifndef		__VAR_MANAGER_H__
+# define	__VAR_MANAGER_H__
+
+t_data_serv		get_data_serv(t_data_serv);
+t_select_manager	get_select_manager(t_select_manager);
+
+#endif		/* !__VAR_MANAGER_H__ */

5e1632bc98237e569e81fa3f044aaf26f328329a
Mister Jingles <ulric.lefevre@gmail.com>
2012-06-30 14:24:45 +0200

    status de l'oeuf

--- Server/src/func_cleaner_1.c
+++ Server/src/func_cleaner_1.c
@@ -5,7 +5,7 @@
 ** Login   <lefevr_u@epitech.net>
 ** 
 ** Started on  Sat Jun 23 20:16:57 2012 ulric lefevre
-** Last update Fri Jun 29 11:53:59 2012 benjamin businaro
+** Last update Fri Jun 29 20:38:20 2012 ulric lefevre
 */
 
 #define		_GNU_SOURCE
@@ -88,15 +88,7 @@ int		action_cleaner(void *ptr, size_t s)
 int		egg_cleaner(void *ptr, size_t s)
 {
   (void)s;
-  if (!((t_egg)ptr)->timeout.tv_sec)
-    return (1);
-  return (0);
-}
-
-int		incant_cleaner(void *ptr, size_t s)
-{
-  (void)s;
-  if (!((t_incant)ptr)->timeout.tv_sec)
+  if (!((t_egg)ptr)->status)
     return (1);
   return (0);
 }

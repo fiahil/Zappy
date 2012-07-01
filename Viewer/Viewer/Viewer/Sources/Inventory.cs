0890af345e20f59d8374f53781adb01924f38740
Fiahil <fiahil@gmail.com>
2012-07-01 16:20:59 +0200

    affichage des éléments sur la map

--- Viewer/Viewer/Viewer/Sources/Inventory.cs
+++ Viewer/Viewer/Viewer/Sources/Inventory.cs
@@ -7,5 +7,34 @@ namespace Viewer.Sources
 {
     class Inventory
     {
+        public uint nourriture;
+        public uint linemate;
+        public uint deraumere;
+        public uint sibur;
+        public uint mendiane;
+        public uint phiras;
+        public uint thystame;
+
+        public Inventory()
+        {
+            this.nourriture = 0;
+            this.linemate = 0;
+            this.deraumere = 0;
+            this.sibur = 0;
+            this.mendiane = 0;
+            this.phiras = 0;
+            this.thystame = 0;
+        }
+
+        public void setAll(uint n, uint l, uint d, uint s, uint m, uint p, uint t)
+        {
+            this.nourriture = n;
+            this.linemate = l;
+            this.deraumere = d;
+            this.sibur = s;
+            this.mendiane = m;
+            this.phiras = p;
+            this.thystame = t;
+        }
     }
 }

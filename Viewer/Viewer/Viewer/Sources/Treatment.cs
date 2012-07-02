ce97ffe802bba9f26e73def0213db967afef916e
epimarti <pierre.martin.epitech@gmail.com>
2012-07-03 00:28:57 +0200

    Ajout de Treatment.cs

---
+++ Viewer/Viewer/Viewer/Sources/Treatment.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Viewer.Sources
+{
+
+    private delegate void TreatCmd(string a);
+    class Treatment
+    {
+        Dictionary<string, TreatCmd> tab;
+        Map map;
+
+        public void Initialize(Map m)
+        {
+            map = m;
+            tab["msz"] = msz;
+        }
+
+        public void Treat(string cmd)
+        {
+            if (tab.ContainsKey(cmd.Substring(0, 3)))
+            {
+                tab[cmd.Substring(0, 3)](cmd.Substring(4));
+            }
+        }
+
+        private void msz(string a)
+        {
+
+        }
+    }
+}

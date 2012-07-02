377a1f439a2900a92906bdcd7075de6e5f3cecc4
Mister Jingles <ulric.lefevre@gmail.com>
2012-07-02 19:34:54 +0200

    graphic process

--- Server/src/graphic_process_2.c
+++ Server/src/graphic_process_2.c
@@ -3,14 +3,36 @@
 
 #include	"def.h"
 
-void		tna_process(t_graphic g, char *param, t_data_serv ds)
+static int	g_current_id;
+
+static int	cmp_id(void *p, size_t len)
 {
+  t_player	player;
+
+  (void)len;
+  player = *((t_player*)data);
+  if (player->id == g_current_id)
+    return (0);
+  return (1);
 }
 
-void		ppo_process(t_graphic g, char *param, t_data_serv ds)
+void		tna_process(t_graphic mn, char *param, t_data_serv ds)
 {
+  (void)param;
+  (void)ds;
+  tna(mn, ds->team);
 }
 
-void		plv_process(t_graphic g, char *param, t_data_serv ds)
+void		ppo_process(t_graphic mn, char *param, t_data_serv ds)
 {
+  t_iter	*ret;
+
+  g_current_id = atoi(param);
+  if ((ret = list_find_if(ds->player, &cmp_id)))
+    ppo(mn, *((t_player*)ret->data));
+}
+
+void		plv_process(t_graphic mn, char *param, t_data_serv ds)
+{
+  
 }

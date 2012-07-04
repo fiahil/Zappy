feec3e44b2c11d36785fa3017f7353eb5b05f3f3
Sulli <lacheteau.s@gmail.com>
2012-07-04 13:44:37 +0200

    graphic_tools.h

---
+++ Server/headers/graphic_tools.h
@@ -0,0 +1,10 @@
+#ifndef __GRAPHIC_TOOLS_H__
+#define __GRAPHIC_TOOLS_H__
+
+int	cmp_id(void *, size_t);
+int	get_current_id(int);
+int	str_is_num(char *);
+int	epur_str(char *, char);
+char	*get_params(char *);
+
+#endif

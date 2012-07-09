d3df78b717e46b8bbac7defe44c503090218c661
busina_b <benjamin.businaro@gmail.com>
2012-07-09 12:00:10 +0200

    AI recolte incant

---
+++ AI/fsmSurvival.ml
@@ -0,0 +1,40 @@
+(*
+ * Busina
+ * 07.07.2012
+ *)
+
+let rec survival () =
+  let default = function
+    | 0 -> Bridge.push Bridge.Avance
+    | 1 -> Bridge.push Bridge.Droite
+    | _ -> Bridge.push Bridge.Gauche
+  in
+  let action = function
+    | -1 ->
+      begin
+	default (Random.int 3);
+	Bridge.push Bridge.Avance
+      end
+    | ps ->
+      begin
+	FsmBase.move ps;
+	FsmBase.gather_all ();
+	if (FsmIncant.test_incant ()) then
+	  begin
+	    Bridge.push Bridge.Incantation;
+	    ignore (Unix.select [] [] [] 25.)
+	  end
+      end
+  in
+  action (FsmBase.find Inventory.Nourriture);
+  Printf.printf "Test :: Un cycle and my lvl is %d\n" !FsmBase.plvl;
+  Printf.printf "      : and my inventory is like = \n";
+  Printf.printf "      : - nourriture : %2d units\n" !FsmBase.piv.Inventory.nourriture;
+  Printf.printf "      : - linemate   : %2d units\n" !FsmBase.piv.Inventory.linemate;
+  Printf.printf "      : - deraumere  : %2d units\n" !FsmBase.piv.Inventory.deraumere;
+  Printf.printf "      : - sibur      : %2d units\n" !FsmBase.piv.Inventory.sibur;
+  Printf.printf "      : - mendiane   : %2d units\n" !FsmBase.piv.Inventory.mendiane;
+  Printf.printf "      : - phiras     : %2d units\n" !FsmBase.piv.Inventory.phiras;
+  Printf.printf "      : - thystame   : %2d units\n" !FsmBase.piv.Inventory.thystame;
+  flush stdout;
+  survival ()
---
+++ AI/fsmSurvival.mli
@@ -0,0 +1,6 @@
+(*
+ * Busina
+ * 07.07.2012
+ *)
+
+val survival : unit -> unit

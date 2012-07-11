f373e53d44252505deb372a2b66b9535308bb9f0
Fiahil <fiahil@gmail.com>
2012-07-11 16:16:39 +0200

    FINI

--- AI/broadcast_ic.ml
+++ AI/broadcast_ic.ml
@@ -6,36 +6,116 @@
 let ic_fp = ref ""
 let ic_id = ref (Random.int 100000)
 
+let move = function
+  | 0           -> true
+  | 1           ->
+      begin
+        Bridge.push (Bridge.Avance);
+        false
+      end
+  | 2           ->
+      begin
+        Bridge.push (Bridge.Avance);
+        Bridge.push (Bridge.Gauche);
+        Bridge.push (Bridge.Avance);
+        false
+      end
+  | 3           ->
+      begin
+        Bridge.push (Bridge.Gauche);
+        Bridge.push (Bridge.Avance);
+        false
+      end
+  | 4           ->
+      begin
+        Bridge.push (Bridge.Gauche);
+        Bridge.push (Bridge.Avance);
+        Bridge.push (Bridge.Gauche);
+        Bridge.push (Bridge.Avance);
+        false
+      end
+  | 5           ->
+      begin
+        Bridge.push (Bridge.Gauche);
+        Bridge.push (Bridge.Gauche);
+        Bridge.push (Bridge.Avance);
+        false
+      end
+  | 6           ->
+      begin
+        Bridge.push (Bridge.Droite);
+        Bridge.push (Bridge.Avance);
+        Bridge.push (Bridge.Droite);
+        Bridge.push (Bridge.Avance);
+        false
+      end
+  | 7           ->
+      begin
+        Bridge.push (Bridge.Droite);
+        Bridge.push (Bridge.Avance);
+        false
+      end
+  | 8           ->
+      begin
+        Bridge.push (Bridge.Avance);
+        Bridge.push (Bridge.Droite);
+        Bridge.push (Bridge.Avance);
+        false
+      end
+  | _           -> false
+
 let rec moving = function
   | Broadcast.Icz id    ->
       if id = !ic_fp then
-        print_endline "OK"
+        if move (Broadcast.gd ()) then
+          Broadcast.bc (Broadcast.Icr (!ic_fp, !ic_id, true))
+        else
+          moving (Broadcast.pp Bridge.pull)
       else
         moving (Broadcast.pp Bridge.pull)
   | _                   -> ()
 
-let rec test_ici () =
-  match Broadcast.pp Bridge.pull with
+let rec test_ici = function
     | Broadcast.Ici (fp, l)     ->
         if fp = !ic_fp && List.exists (fun v -> v = !ic_id) l then
           true
         else
-          test_ici ()
+          test_ici (Broadcast.pp Bridge.pull)
     | _                 -> false
 
-let rec test_rcp () =
-  match Broadcast.pp Bridge.take with
+let rec test_rcp = function
     | Broadcast.Icq (fp, lvl)   ->
         begin
           ic_fp := fp;
           lvl
         end
-    | Broadcast.Err ""          -> 0
-    | _                 -> test_rcp ()
+    | Broadcast.Err ""  -> 0
+    | _                 -> test_rcp (Broadcast.pp Bridge.take)
+
+let test_launch v = 
+  let rec aux n = function
+    | Broadcast.Icl id  ->
+        if id = !ic_fp && n = 1 then
+          ()
+        else if (id = !ic_fp) then
+          aux (n + 1) (Broadcast.pp Bridge.pull)
+        else
+          aux n (Broadcast.pp Bridge.pull)
+    | Broadcast.Ica id  ->
+        if id = !ic_fp then
+          ()
+        else
+          aux n (Broadcast.pp Bridge.pull)
+    | _                 -> aux n (Broadcast.pp Bridge.pull)
+  in
+  aux 0 v
 
 let engage () =
   begin
     Broadcast.bc (Broadcast.Icr (!ic_fp, !ic_id, false));
-    if test_ici () then
-      moving (Broadcast.pp Bridge.pull)
+    if test_ici (Broadcast.pp Bridge.pull) then
+      begin
+        moving (Broadcast.pp Bridge.pull);
+        test_launch (Broadcast.pp Bridge.pull)
+      end
   end
--- AI/fsmLaunch.ml
+++ AI/fsmLaunch.ml
@@ -31,7 +31,7 @@ let run () =
     (* !func !func_param; *)
       IncantManager.init_incant_id ();
       Bridge.init ();
-      if Broadcast_ic.test_rcp () = !FsmBase.plvl && FsmIncant.test_food () then
+      if Broadcast_ic.test_rcp (Broadcast.pp Bridge.take) = !FsmBase.plvl && FsmIncant.test_food () then
         Broadcast_ic.engage ()
       else if (FsmIncant.test_food () = false) then
 	FsmSurvival.survival []

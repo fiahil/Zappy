33a8649879fa228bbe9f3345e97dfe32105f96e8
busina_b <benjamin.businaro@gmail.com>
2012-07-14 20:43:35 +0200

    new launcher

--- AI/launcher.sh
+++ AI/launcher.sh
@@ -4,7 +4,7 @@
 ## Login   <busina_b@epitech.net>
 ## 
 ## Started on  Sat Jul 14 18:04:26 2012 benjamin businaro
-## Last update Sat Jul 14 18:13:07 2012 benjamin businaro
+## Last update Sat Jul 14 20:12:27 2012 benjamin businaro
 ##
 #!/bin/sh
 ##
@@ -13,13 +13,15 @@
 ## benjamin businaro
 ## Sat Jul 14 2012
 
-if [ $# -lt 3 ]
+if [ $# -lt 4 ]
 then
-    echo "Usage : ./launcher.sh <team_name> <host> <port>"
+    echo "Usage : ./launcher.sh <nb_client> <team_name> <host> <port>"
 else
-    for i in {0..7}
+    j=0
+    while [ $j -lt $1 ]
     do
-	./ai_client -n $1 -h $2 -p $3 -s&
+	./ai_client -n $2 -h $3 -p $4 -s&
+        j=$(($j+1))
     done
 fi
 

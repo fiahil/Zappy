93eaa636334fd319664c3ff80747c3d2efcbfde5
busina_b <benjamin.businaro@gmail.com>
2012-07-14 15:47:57 +0200

    Ai full

--- AI/fsmLaunch.mli
+++ AI/fsmLaunch.mli
@@ -3,20 +3,6 @@
  * 10.07.2012
  *)
 
-(*
- * Set state function
- *)
-val set : (Inventory.resources list -> unit) -> unit
-
-(*
- * Set param
- *)
-val set_param : Inventory.resources list -> unit
-
-(*
- * reset function to last state
- *)
-val reset : unit -> unit
 
 (*
  * Run Finite state machine

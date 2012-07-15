378175447098585f72d8e6575f8bacc64e0d1ca8
Mister Jingles <ulric.lefevre@gmail.com>
2012-07-15 11:10:21 +0200

    Suppression des autotests

--- Server/autotest/burstM.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-for i in {0..100} ;do
-  ./stringM.sh | nc 10.19.253.175 4444 > /dev/null &
-done
-killall nc
--- Server/autotest/string.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-echo "Poney"
-tr -cd '[:alnum:]\n' < /dev/urandom | head -c1024
